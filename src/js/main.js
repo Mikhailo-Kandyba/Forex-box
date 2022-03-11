2beaf9b25f023e86e5671e8b57936180de26546f
Mikhailo <mikhailo.kandyba0@gmail.com>
2022-03-11 17:00:15 +0300

    desktop version

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+
+$(".btn-menu").on('click', function(e) {
+    e.preventDefault();
+    $(this).toggleClass("active");
+    $(".menu-navigation-wrap").toggleClass('show-menu');
+    $("body").toggleClass('overflow-hidden');
+    $("body").toggleClass('background');
+});
