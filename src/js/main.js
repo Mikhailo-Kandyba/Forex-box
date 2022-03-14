ae1a6fa1bb0551cacab5f75d1a86e9a3a7dbef48
Mikhailo <mikhailo.kandyba0@gmail.com>
2022-03-14 13:54:06 +0300

    account-page

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,58 @@ $(".btn-menu").on('click', function(e) {
     $("body").toggleClass('overflow-hidden');
     $("body").toggleClass('background');
 });
+
+// When the user clicks on div, open the popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
+$(function() {
+
+    //BEGIN
+    $(".accordion__title").on("click", function(e) {
+
+        e.preventDefault();
+        var $this = $(this);
+
+        if (!$this.hasClass("accordion-active")) {
+            $(".accordion__content").slideUp(400);
+            $(".accordion__title").removeClass("accordion-active");
+            $('.accordion__arrow').removeClass('accordion__rotate');
+        }
+
+        $this.toggleClass("accordion-active");
+        $this.next().slideToggle();
+        $('.accordion__arrow',this).toggleClass('accordion__rotate');
+    });
+    //END
+
+});
+
+
+$( function() {
+    $.widget( "custom.iconselectmenu", $.ui.selectmenu, {
+        _renderItem: function( ul, item ) {
+            var li = $( "<li>" ),
+                wrapper = $( "<div>", { text: item.label } );
+
+            if ( item.disabled ) {
+                li.addClass( "ui-state-disabled" );
+            }
+
+            $( "<span>", {
+                style: item.element.attr( "data-style" ),
+                "class": "ui-icon " + item.element.attr( "data-class" )
+            })
+                .appendTo( wrapper );
+
+            return li.append( wrapper ).appendTo( ul );
+        }
+    });
+
+    $( "#filesB" )
+        .iconselectmenu()
+        .iconselectmenu( "menuWidget" )
+        .addClass( "ui-menu-icons customicons" );
+} );
